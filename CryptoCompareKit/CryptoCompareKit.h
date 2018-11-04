1038bfc015fa4cfb0b2b865a94c830a36163ba81
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2018-11-04 12:08:17 +0200

    Initial Commit

---
+++ CryptoCompareKit/CryptoCompareKit.h
@@ -0,0 +1,19 @@
+//
+//  CryptoCompareKit.h
+//  CryptoCompareKit
+//
+//  Created by Spiros Gerokostas on 04/11/2018.
+//  Copyright © 2018 Spiros Gerokostas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptoCompareKit.
+FOUNDATION_EXPORT double CryptoCompareKitVersionNumber;
+
+//! Project version string for CryptoCompareKit.
+FOUNDATION_EXPORT const unsigned char CryptoCompareKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoCompareKit/PublicHeader.h>
+
+
