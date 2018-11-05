d3a6670af9550f5d8da8215b775fce4ae55e2cc1
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2018-11-05 21:07:32 +0200

    Adding historical

---
+++ CryptoCompareKit/Endpoints/Historical.swift
@@ -0,0 +1,12 @@
+//
+//  Historical.swift
+//  CryptoCompareKit
+//
+//  Created by Spiros Gerokostas on 05/11/2018.
+//  Copyright © 2018 Spiros Gerokostas. All rights reserved.
+//
+
+
+extension CryptoCompare {
+  
+}
