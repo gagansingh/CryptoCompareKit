7bdb6aee2665ff186bfcdf7760608f1204d18711
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2018-12-25 10:26:19 +0200

    Changes to endpoints

--- CryptoCompareKit/Endpoints/List.swift
+++ CryptoCompareKit/Endpoints/List.swift
@@ -5,9 +5,10 @@
 //  Created by Spiros Gerokostas on 04/11/2018.
 //  Copyright © 2018 Spiros Gerokostas. All rights reserved.
 //
-
+// swiftlint:disable force_unwrapping
 extension CryptoCompare {
-    public func coinList(success: SuccessResponse<CoinList>?, failure: FailureResponse?) {
-        request("/all/coinlist", success: success, failure: failure)
+    public func coinList(success: SuccessResponse<CoinList>?,
+                         failure: FailureResponse?) -> Request {
+        return request("/all/coinlist", success: success, failure: failure)!
     }
 }
