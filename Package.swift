bbf31c2f39b8e30fb102d88be63648bd57b590c3
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-04-19 13:04:32 +0300

    build: update package.swift configuration

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "CryptoCompareKit",
-    // platforms: [.iOS("10.0"), .macOS("10.12"), tvOS("10.0"), .watchOS("3.0")],
+    platforms: [.iOS(.v10)],
     products: [
         .library(name: "CryptoCompareKit", targets: ["CryptoCompareKit"])
     ],
