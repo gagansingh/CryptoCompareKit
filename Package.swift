b1ae2c7304c63724fbc3c51e0d6ce875b46b9b41
Gagan Singh <gsingh@resy.com>
2020-06-06 01:27:23 -0400

    add mac support

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CryptoCompareKit",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v10), .macOS(.v10_14)],
     products: [
         .library(name: "CryptoCompareKit", targets: ["CryptoCompareKit"])
     ],
